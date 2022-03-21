a617c641b32b0409eedd4c315ed6b9082ff9e15a
Wesley Enrique <99751360+WesleyEnrique@users.noreply.github.com>
2022-03-20 22:32:04 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-HT = float(input("Quantidade de horas trabalhadas no mês: "))
-VH = float(input("Valor da hora trabalhada: "))
-PD = float(input("Qual o percentual de desconto: "))
-salarioBruto = HT*VH
-Desconto = (PD/100)*salarioBruto
-liquido = salarioBruto - Desconto
-print(" As horas trabalhadas são {},\n salario bruto é {},\n total de desconto {}\n e salario liquido {}".format(HT, salarioBruto, Desconto, liquido))
+temperatuda = float(input("Qual a temperatura? "))
+C = (temperatuda - 32) * 5/9
+print("A temperatuda em graus celcius será", C)
